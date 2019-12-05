a093206e6f31ade61d892afb8947c924a58301d4
Daniil Gladchenko <daniil.gladchenko@firstlinesoftware.com>
2019-12-05 15:49:32 +0300

    Compression service added

---
+++ GZip/CompressionService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GZip
+{
+    public class CompressionService
+    {
+        public void Compress(FileInfo file)
+        {
+            var arrays = Arrays(file);
+
+            try
+            {
+                //поток для записи сжатого файла
+                using (FileStream targetStream = File.Create(file.FullName + ".gz"))
+                {
+                    //поток для компрессии
+                    using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        using (Stream syncStream = Stream.Synchronized(compressionStream))
+                        {
+                            for (int i = 0; i < file.Length; i += 1000000)
+                            {
+                                Thread thread = new Thread(() =>
+                                {
+                                    byte[] buffer = arrays.Dequeue();
+                                    syncStream.Write(buffer, 0, buffer.Length);
+                                });
+                                thread.Start();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.InnerException?.Message ?? ex.Message}");
+            }
+
+            var info = new FileInfo(file.FullName + ".gz");
+            Console.WriteLine($"Compressed {file.Name} from {file.Length.ToString()} to {info.Length.ToString()}");
+        }
+
+        public void Decompress(FileInfo file)
+        {
+            try
+            {
+                //поток для чтения сжатого файла
+                using (FileStream sourceStream = file.OpenRead())
+                {
+                    string currentFileName = file.FullName;
+                    string newFileName = currentFileName.Remove(currentFileName.Length - file.Extension.Length);
+
+                    //поток для записи восстановленного файла
+                    using (FileStream targetStream = File.Create(newFileName))
+                    {
+                        //поток для декомпрессии
+                        using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                        {
+                            using (Stream syncStream = Stream.Synchronized(decompressionStream))
+                            {
+                                byte[] buffer = new byte[1000000];
+                                for (int i = 0; i < sourceStream.Length; i += 1000000)
+                                {
+                                    syncStream.Read(buffer, 0, buffer.Length);
+                                    Thread thread = new Thread(() =>
+                                    {
+                                        targetStream.Write(buffer, 0, buffer.Length);
+                                    });
+                                    thread.Start();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.InnerException.Message ?? ex.Message}");
+            }
+
+            Console.WriteLine($"Decompressed {file.Name}");
+        }
+
+        private static Queue<byte[]> Arrays(FileInfo file)
+        {
+            Queue<byte[]> arrays = new Queue<byte[]>();
+            using (FileStream sourceStream = file.OpenRead())
+            {
+                for (int i = 0; i < sourceStream.Length; i += 1000000)
+                {
+                    byte[] buffer = new byte[1000000];
+                    sourceStream.Read(buffer, 0, buffer.Length);
+                    arrays.Enqueue(buffer);
+                }
+            }
+
+            return arrays;
+        }
+    }
+}
--- GZip/Program.cs
+++ GZip/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace GZip
 {
@@ -10,6 +9,47 @@ namespace GZip
     {
         static void Main(string[] args)
         {
+            var watch = Stopwatch.StartNew();
+            var compressionService = new CompressionService();
+            var command = args[0];
+            var filePath = args[1];
+
+            try
+            {
+                if (string.Equals(command, "compress") || string.Equals(command, "decompress"))
+                {
+                    var file = new FileInfo(filePath);
+                    if (string.Equals(command, "compress"))
+                    {
+                        Console.WriteLine("Compressing started...");
+                        compressionService.Compress(file);
+                    }
+                    else if (string.Equals(command, "decompress"))
+                    {
+                        if (file.Extension != ".gz")
+                        {
+                            Console.WriteLine("Incorrect file format");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Decompressing started...");
+                            compressionService.Decompress(file);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Your command is incorrect. Please input correct command 'compress' or 'decompress'");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.InnerException?.Message ?? ex.Message} \n Please check your data and try again.");
+            }
+
+            watch.Stop();
+            Console.WriteLine($"Elapsed time: {watch.Elapsed.Minutes}:{watch.Elapsed.Seconds}");
+            Console.ReadKey();
         }
     }
 }
